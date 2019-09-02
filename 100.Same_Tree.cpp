3cf1502a6e5dba1fbf0caa50d3d6354d2559175d
hqnddw <173056013@qq.com>
2019-09-02 11:05:57 +0800

    9/1

--- 100.Same_Tree.cpp
+++ 100.Same_Tree.cpp
@@ -25,7 +25,7 @@ public:
 };
 
 //迭代
-class Solution {
+class Solution2 {
 public:
     bool isSameTree(TreeNode *p, TreeNode *q) {
         if (!p || !q) return p == q;
@@ -47,5 +47,68 @@ public:
             if (s_q.size() != s_p.size()) return false;
         }
         return s_p.size() == s_q.size();
+    }
+};
+
+//第二次
+class Solution3 {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (!p && !q)
+            return true;
+        if (!p || !q)
+            return false;
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+//迭代
+//两个队列
+class Solution4 {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        queue<TreeNode *> q1;
+        queue<TreeNode *> q2;
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        q1.push(p),
+                q2.push(q);
+        while (!q1.empty() && !q2.empty()) {
+            p = q1.front();
+            q1.pop();
+            q = q2.front();
+            q2.pop();
+            if (p->val != q->val) return false;
+            if (p->left) q1.push(p->left);
+            if (q->left) q2.push(q->left);
+            if (q1.size() != q2.size()) return false;
+            if (p->right) q1.push(p->right);
+            if (q->right) q2.push(q->right);
+            if (q2.size() != q1.size()) return false;
+        }
+        return true;
+    }
+};
+
+//一个队列
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        queue<TreeNode *> queue;
+        queue.push(p);
+        queue.push(q);
+        while (!queue.empty()) {
+            p = queue.front();
+            queue.pop();
+            q = queue.front();
+            queue.pop();
+            if (!p && !q) continue;
+            if (!p || !q || p->val != q->val) return false;
+            queue.push(p->left);
+            queue.push(q->left);
+            queue.push(p->right);
+            queue.push(q->right);
+        }
+        return true;
     }
 };
