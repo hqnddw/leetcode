2614aa404b50b52774de3f1540187d89de9ebc1c
hqnddw <173056013@qq.com>
2019-10-07 20:11:31 +0800

    10/7

---
+++ 43.Multiply-Strings.cpp
@@ -0,0 +1,28 @@
+//
+// Created by hqnddw on 2019/10/6.
+//
+#include <string>
+
+using namespace std;
+
+class Solution1 {
+public:
+    string multiply(string num1, string num2) {
+        int m = num1.size();
+        int n = num2.size();
+        string res(m + n, 0);
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int sum = (num1[i] - '0') * (num2[j] - '0') + (res[i + j + 1] - '0');
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        for (int k = 0; k < res.size(); ++k) {
+            if (res[k] != '0')
+                return res.substr(k);
+        }
+        return "0";
+    }
+};
